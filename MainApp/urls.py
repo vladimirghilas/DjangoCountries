bbe6363d4bca6cf28d61c083a5618b37475441ae
vladimir ghilas <vladghil37@gmail.com>
2025-08-21 22:14:10 +0300

    module 5 and 7 and finish HW

--- MainApp/urls.py
+++ MainApp/urls.py
@@ -3,10 +3,13 @@ from . import views
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('about/',views.about_view, name='about'),
+    path('about/', views.about_view, name='about'),
     path('', views.home_views, name='home'),
     path('countries/list/', views.countries_view, name='countries-list'),
-    path('countries/languages/', views.languages_view, name='languages-list'),
-    path('countries/<str:country_name>/', views.country_detail, name='country-detail'),
+    path('countries-by/<str:letter>/', views.countries_view, name='countries-by-letter'),
+    path('languages/list', views.languages_view, name='languages-list'),
+    path('languages-by/<str:letter>/', views.languages_view, name='languages-by-letter'),
+    path('languages/<str:country_name>/', views.country_detail, name='country-languages'),
+    path('countries/<str:language_name>/', views.language_detail, name='language-countries'),
 
 ]
--- MainApp/views.py
+++ MainApp/views.py
@@ -1,22 +1,61 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from MainApp.models import Country, Language
 
 
 def about_view(request):
     return render(request, 'about.html')
+
+
 def home_views(request):
     return render(request, 'home.html')
 
-def countries_view(request):
-    countries = Country.objects.all().order_by('name')  # все страны из БД
-    return render(request, 'countries.html', {'countries': countries})
+
+def countries_view(request, letter=None):
+    letters = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+    if letter:
+        countries_list = Country.objects.filter(name__startswith=letter).order_by('name')
+    else:
+        countries_list = Country.objects.all().order_by('name')  # все страны из БД
+        # пагинация (10 стран на страницу)
+    paginator = Paginator(countries_list, 9)
+    page_number = request.GET.get("page")  # номер страницы из query params ?page=2
+    countries = paginator.get_page(page_number)
+
+    context = {
+        'countries': countries,
+        "letters": letters,
+        "selected_letter": letter
+    }
+    return render(request, 'countries.html', context)
+
 
 def country_detail(request, country_name):
     country = get_object_or_404(Country, name=country_name)
-    return render(request, 'country_detail.html', {'country': country})
+    return render(request, 'country_languages.html', {'country': country})
 
-def languages_view(request):
-    # Luăm toate limbile din DB
-    languages = Language.objects.all().prefetch_related('countries').order_by('name')
 
-    return render(request, 'lang.html', {'languages': languages})
+def languages_view(request, letter=None):
+    letters = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+    if letter:
+        languages_list = Language.objects.filter(name__startswith=letter).order_by('name')
+    else:
+        languages_list = Language.objects.all().order_by('name')
+
+    paginator = Paginator(languages_list, 6)
+    page_number = request.GET.get("page")  # номер страницы из query params ?page=2
+    languages = paginator.get_page(page_number)
+
+    context = {
+        "letters": letters,
+        'languages': languages,
+        "selected_letter": letter
+    }
+    return render(request, 'languages.html', context)
+
+
+def language_detail(request, language_name):
+    language = get_object_or_404(Language, name=language_name)
+    return render(request, 'language_countries.html', {'language': language})
